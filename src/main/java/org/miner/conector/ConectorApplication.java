70b45988c35be480cf1c3ddb436b1b7ab6b1b07b
Novikov, Ruslan <ruslan.novikov@here.com>
2018-01-08 14:19:51 +0100

    Initial commit

---
+++ src/main/java/org/miner/conector/ConectorApplication.java
@@ -0,0 +1,12 @@
+package org.miner.conector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectorApplication.class, args);
+	}
+}
